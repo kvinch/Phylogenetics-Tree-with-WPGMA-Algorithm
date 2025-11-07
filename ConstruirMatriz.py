7c75eb18bb7310ab2092d8123f7c6e3130fd7fb0
kvin <chanhuangkevin@gmail.com>
2025-11-06 22:52:59 -0500

    Implement Jukes-Cantor correction in calcularDiferencias
    Added Jukes-Cantor correction for distance calculation.

--- ConstruirMatriz.py
+++ ConstruirMatriz.py
@@ -1,5 +1,6 @@
+import math
 
-def calcularDiferencias(A, B):
+def calcularDiferencias(A, B): #Las longitudes deben ser iguales entre sí
     longitud = len(A) #Saca la longitud del OTUS A
     diff = 0 # Número de diferencias, inicia en 0
 
@@ -8,7 +9,16 @@ def calcularDiferencias(A, B):
             diff += 1
             
     diff = diff/longitud #Se revisan el porcentaje de diferencias
-    return round(diff, 2) #Se redondea para 2 decimales
+    
+    #Jukes-Cantor 1969, se calcula la corrección con la fórmula -3/4 * log(1 - 4/3*f)
+    if (diff >= 0.75):
+        print("No se puede calcular, la distancia saldrá negativa.")
+        return None
+    
+
+    d = - 3/4 * math.log(1- 4/3 * diff)
+
+    return round(d, 2) #Se redondea para 2 decimales
 
 def construirMatriz(otus):
     n = len(otus) #Se saca el largo de las secuencias
