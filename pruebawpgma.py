f2dd7c8490bb83dd9631cbab224f576a05a00cd7
kvin <chanhuangkevin@gmail.com>
2025-11-08 19:31:01 -0500

    Implement tree visualization with error handling
    Added visualization for WPGMA tree using Bio.Phylo and matplotlib.

--- pruebawpgma.py
+++ pruebawpgma.py
@@ -163,7 +163,18 @@ wpgma = WPGMA(mat)
 a = wpgma.wpgma() 
 print(a)
 
-
+try:  #Intentamos importar las librerías necesarioas para la visualización
+	import io
+	from Bio import Phylo
+	import matplotlib.pyplot as plt
+	newick = io.StringIO(a)
+	tree = Phylo.read(newick, "newick")
+	fig = plt.figure(figsize=(10, 6), dpi=100)
+	axes = fig.add_subplot(1, 1, 1)
+	Phylo.draw(tree, axes=axes)
+	plt.show() #Muestra el gráfico en una ventana aparte
+except ImportError:  #Las librerías no están instaladas
+	print("No se puede visualizar, porque las librerías no están instaladas")
 
 
 
